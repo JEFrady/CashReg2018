9d232379420d211d72f913220a152cf1e9a996d9
JEFrady <jamie.e.halliday@gmail.com>
2018-09-15 11:07:14 -1000

    Server up, postico connected, structure ready. Next step to seed knex, migrate code to knex

---
+++ Desktop/JS/art-express-2/app.js
@@ -0,0 +1,36 @@
+/// App
+const express = require('express');
+const app = express();
+const bp = require('body-parser');
+const expressHbs = require('express-handlebars');
+
+const artRoute = require('./routes/articles.js');
+const prodRoute = require('./routes/products.js');
+
+const knex = require('./knex/knex.js');
+const methOver = require('method-override');
+
+
+/// USE
+app.use(express.static( __dirname + '/public'));
+app.use(bp.urlencoded({ extended: true }));
+app.use(methOver('_method'));
+app.use('/products', prodRoute);
+app.use('/articles', artRoute);
+
+
+// app.use(express.static('public'));
+app.engine('.hbs', expressHbs({ defaultLayout: 'main', extname: '.hbs'}));
+app.set('view engine', '.hbs');
+
+///HOME//////////////////////////////////////////////////
+app.get('/', (req, res) => {
+    console.log('Render home page')
+    res.render('home')
+})
+
+
+/// LISTEN
+app.listen(process.env.EXPRESS_CONTAINER_PORT, () => {
+    console.log(`Started app on port: ${process.env.EXPRESS_CONTAINER_PORT}`);
+  });
